eeda1ea1e8c13f070e3115149cf2563e66fd625b
manishbisht2000 <manishbisht2000@gmail.com>
2025-06-03 23:51:03 +0530

    Completed the API's to get, create, update & delete products

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import { connectDB } from "./config/db.js"
-import Product from "./models/product.model.js"
+import productRoutes from "./routes/product.route.js"
 
 dotenv.config()
 
@@ -9,24 +9,7 @@ const app = express()
 
 app.use(express.json())
 
-app.post("/api/products", async (req, res) => {
-    const product = req.body
-
-    if(!product.name || !product.price || !product.image) {
-        return res.status(400).json({success:false, message: "Please peovide all fields"})
-    }
-
-    const newProduct = new Product(product)
-
-    try {
-        await newProduct.save();
-        res.status(201).json({success:true, data:newProduct})
-    } catch (error) {
-        console.error("Error in create product", error.message)
-        res.status(500).json({success:false, message: "Server error"})
-        
-    }
-})
+app.use("/api/products", productRoutes)
 
 console.log(process.env.MONGO_URI)
 
