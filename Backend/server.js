54e0ff0f413251ea49a5a1f717054f6a69883631
manishbisht2000 <manishbisht2000@gmail.com>
2025-06-04 15:55:58 +0530

    React setup with Chakra UI

--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,7 @@ import productRoutes from "./routes/product.route.js"
 dotenv.config()
 
 const app = express()
+const PORT = process.env.PORT || 5000
 
 app.use(express.json())
 
@@ -13,8 +14,8 @@ app.use("/api/products", productRoutes)
 
 console.log(process.env.MONGO_URI)
 
-app.listen(5000, () =>{
+app.listen(PORT, () =>{
     connectDB()
-    console.log("Server is running at http://localhost:5000")
+    console.log("Server is running at http://localhost:"+ PORT)
 
 })
